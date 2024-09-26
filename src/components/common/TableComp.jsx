86c2c791a02d4f527d0db850c276e0e466d25bed
Muthu <madasamy939@gmail.com>
2024-09-26 14:15:30 +0530

    table ui bug fix

--- src/components/common/TableComp.jsx
+++ src/components/common/TableComp.jsx
@@ -20,7 +20,7 @@ const TableComp = ({name, columns,filteredData,setSearchText}) => {
                         placeholder='Search products...'
                         className='bg-gray-700 text-white placeholder-gray-400 
                         rounded-lg pl-10 pr-4 py-2 focus:outline-none focus:ring-2
-                         focus:ring-blue-500'
+                         focus:ring-blue-500 w-[180px] sm:w-full'
                         onChange={(e) => setSearchText(e.target.value)} />
                     <Search className='absolute left-3 top-2.5 text-gray-400' size={18} />
                 </div>
